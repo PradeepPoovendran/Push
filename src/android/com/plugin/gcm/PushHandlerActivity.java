440061c7c17df45e78579f0080f78885ddf5f6fc
PradeepPoovendran <poopradeepmca@gmail.com>
2017-11-06 19:36:05 +0530

    Update PushHandlerActivity.java

--- src/android/com/plugin/gcm/PushHandlerActivity.java
+++ src/android/com/plugin/gcm/PushHandlerActivity.java
@@ -1,5 +1,7 @@
 package com.plugin.gcm;
 
+
+import java.io.IOException;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -7,30 +9,118 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.media.AudioManager;
+
+import android.content.res.AssetFileDescriptor;
+
+import com.smartfmone.smartfmone.v1.R;
 
 public class PushHandlerActivity extends Activity
 {
 	private static String TAG = "PushHandlerActivity"; 
 
-	/*
-	 * this activity will be started if the user touches a notification that we own. 
-	 * We send it's data off to the push plugin for processing.
-	 * If needed, we boot up the main activity to kickstart the application. 
-	 * @see android.app.Activity#onCreate(android.os.Bundle)
-	 */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		Log.v(TAG, "onCreate");
 
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        
+		
+		
+		
+				Bundle extrass = getIntent().getExtras();
+				Bundle originalExtras = extrass.getBundle("pushBundle");
+				
+				String PushadditionalData= originalExtras.getString("additionalData");
+		
+		if(PushadditionalData.equals("Security"))		
+		{
+			final MediaPlayer ring = MediaPlayer.create(getApplicationContext(), R.raw.silent);
+			ring.setVolume(1.0f,1.0f);
+			/*
+			AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE); 
+			int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+			
+			
+			int maxVolume = 50;
+			float log1=(float)(Math.log(maxVolume-currentVolume)/Math.log(maxVolume)); 
+			audio.setVolume(1-log1);
+			*/
+			
+			
+			ring.setLooping(false);        
+			ring.start();
+			
+			
+			
+			 new AlertDialog.Builder(this).setMessage(originalExtras.getString("message")).setNeutralButton("Close", new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+				ring.stop();
+				//forceMainActivityReload();
+finish();
+
+            }
+        }).show();
+		
+		}
+		else
+		{
+			final MediaPlayer ring = MediaPlayer.create(getApplicationContext(), R.raw.siren); 
+/*
+			AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE); 
+			int currentVolume = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
+			
+			int maxVolume = 50;
+			
+			float log1=(float)(Math.log(maxVolume-currentVolume)/Math.log(maxVolume)); 
+			audio.setVolume(1-log1);	
+*/
+			ring.setVolume(1.0f,1.0f);
+			ring.setLooping(true);        
+			ring.start();
+			
+			
+				
+				//originalExtras.getString("message");
+			 new AlertDialog.Builder(this).setMessage(""+originalExtras.getString("message")).setNeutralButton("Close", new OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+				ring.stop();
+				//forceMainActivityReload();
+finish();
+
+            }
+        }).show();
+		}
+		
+		
 		boolean isPushPluginActive = PushPlugin.isActive();
 		processPushBundle(isPushPluginActive);
 
-		finish();
+		
 
 		if (!isPushPluginActive) {
-			forceMainActivityReload();
+			//forceMainActivityReload();
 		}
 	}
 
